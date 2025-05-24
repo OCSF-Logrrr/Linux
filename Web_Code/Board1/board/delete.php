d55cc808e2649b0e732b346322cb22b683a34d50
Padnyo <167075494+Padnyo@users.noreply.github.com>
2025-05-24 16:50:42 +0900

    Update delete.php

--- Web_Code/Board1/board/delete.php
+++ Web_Code/Board1/board/delete.php
@@ -1,5 +1,5 @@
 <?php
-    $conn=mysqli_connect("localhost","keshu","1234","DB");
+    $conn=mysqli_connect("localhost","test","1234","DB");
     $idx=$_POST["idx"];
     $userid=$_COOKIE['userid'];
     $sql="SELECT count(*) as cnt FROM board where idx='$idx' and userid='$userid'";
