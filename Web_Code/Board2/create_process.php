8dadcd9409e3bfa783834d6035b6c7e147daba4b
thdwlgus <114208277+thdwlgus@users.noreply.github.com>
2025-05-24 17:26:19 +0900

    Update create_process.php

--- Web_Code/Board2/create_process.php
+++ Web_Code/Board2/create_process.php
@@ -3,22 +3,24 @@ $conn = mysqli_connect("localhost","webuser","1234","db_board");
 $URL = './index.php';         
 
 $sql ="INSERT INTO board
-        (title, content, date, hit, id, password) 
+        (title, content, date, hit, id) 
         values(
         '{$_POST['title']}',
         '{$_POST['content']}',
         NOW(),
          0,
-        '{$_POST['name']}',
-        '{$_POST['password']}'
+        '{$_POST['name']}'
         )";
 $result = mysqli_query($conn,$sql); 
 
-if($result === false){
+if($result){
+    ?> <script>
+        alert("<?php echo "게시글이 등록되었습니다."?>");
+        location.replace("<?php echo $URL?>");
+        </script>
+<?php
+} else {
     echo "문제가 생겼습니다. 다시 시도해 주세요.";
-    
-} else{
-    echo "게시글이 등록되었습니다. <a href='$URL'>돌아가기</a>";
 }
-
+mysqli_close( $conn );
 ?>
