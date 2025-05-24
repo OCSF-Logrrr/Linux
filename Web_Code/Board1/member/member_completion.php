d0283f41310ed44abab024c738947a239a442fb7
Padnyo <167075494+Padnyo@users.noreply.github.com>
2025-05-24 16:20:57 +0900

    Create member_completion.php

---
+++ Web_Code/Board1/member/member_completion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link href="/css/member_completion.css" rel="stylesheet" type="text/css">
+    <title>게시판-회원가입</title>
+    <h1>회원가입 완료!</h1>
+</head>
+<body>
+    <a href="../index.php"><button type="button" class="button">홈으로</button></a>
+    <a href="/member/login.php"><button type="button" class="button2">로그인</button></a>
+</body>
+</html>
