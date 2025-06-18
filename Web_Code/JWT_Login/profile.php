36547d21a4310ae58d7a6bdd8e3e49c2cedb9690
Padnyo <167075494+Padnyo@users.noreply.github.com>
2025-06-19 00:31:47 +0900

    Create profile.php

---
+++ Web_Code/JWT_Login/profile.php
@@ -0,0 +1,53 @@
+<link href="/profile.css" rel="stylesheet" type="text/css">
+<?php
+$conn=mysqli_connect("localhost","test","1234","JWT");
+$sql="select * from test";
+$result=mysqli_query($conn,$sql);
+$num=mysqli_num_rows($result);
+require 'JWT_class.php';
+
+$jwt = new JWT();
+
+if (!isset($_COOKIE['token'])) {
+    echo "<script>alert('로그인 후 이용 가능');";
+    echo "location='/login.php';</script>";
+    exit;
+}
+
+$token = $_COOKIE['token'];
+
+if ($jwt->dehashing($token)) {
+    list($header, $payload, $signature) = explode('.', $token);
+    $payload = json_decode(base64_decode($payload),true);
+    if (($payload['name'])=='admin'){
+?>
+    <h1>관리자 정보 및 회원 정보</h1>
+    <h3>name: admin</h3>
+        <div>
+        <table class="list" align="center" border="1">
+        <thead>
+            <tr>
+            <th>username</th>
+            <th>password</th>
+            </tr>
+        </thead>
+        <?php
+            for($i=1;$i<=$num;$i++){
+            $row=mysqli_fetch_object($result);
+        ?>
+        <tbody>
+        <tr> 
+            <td style="width: 10%;"><?=$row->name?></td>
+            <td style="width: 10%;"><?=$row->pw?></td>
+        </tr>
+        <?php
+            }mysqli_close($conn);
+    } else{
+?>
+        <h1>사용자 정보</h1>
+        <h3>name: <?= $payload['name']; ?></h3>
+        <div class='logout'><a href='/login.php'><button>로그아웃</button></a></div>
+<?php    
+    }
+}
+?>
