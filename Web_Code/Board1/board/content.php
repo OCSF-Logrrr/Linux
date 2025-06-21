5686960b5d8fd9c50ea512fa8dd20931d7c28060
ieuns <les070207@naver.com>
2025-06-21 14:04:45 +0900

    Update content.php

--- Web_Code/Board1/board/content.php
+++ Web_Code/Board1/board/content.php
@@ -1,32 +1,59 @@
 <?php
-    $conn=mysqli_connect("localhost","test","1234","DB");
-    $idx=$_GET["idx"];
-    $sql="select * from board where idx=$idx";
-    $result=mysqli_query($conn,$sql);
-    $row=mysqli_fetch_object($result);
-    $hit_sql="UPDATE board set hit=hit+1 where idx='$idx'";
-    mysqli_query($conn,$hit_sql);
+    $conn = mysqli_connect("3.39.142.255", "root", "1234", "board");
+
+    $idx = $_GET["idx"];
+
+    $sql = "SELECT * FROM board WHERE idx = $idx";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_object($result);
+
+    $hit_sql = "UPDATE board SET hit = hit + 1 WHERE idx = '$idx'";
+    mysqli_query($conn, $hit_sql);
+
+    $file_name = $row->file_name;
+    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    $is_image = in_array($file_ext, ['jpg', 'jpeg', 'png', 'gif', 'webp']);
+
+    $upload_path = "/webapi/files/";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
+        <base href="/webapi/">
     <link href="/css/content.css" rel="stylesheet" type="text/css">
-    <title>게시판-<?=$row->userid?>님의 글</title>
-    <h1><?=$row->userid?>님의 글</h1>
+    <title>게시판 - <?=htmlspecialchars($row->userid)?>님의 글</title>
 </head>
 <body>
+    <h1><?=htmlspecialchars($row->userid)?>님의 글</h1>
     <table class="list">
-        <tr><td class="title"><?=$row->title?></td></tr>
-        <tr><td class="file">업로드 된 파일 : <a href="/board/download.php?file=<?=$row->file_name?>" download><?=$row->file_name?></a></td></tr>
-        <tr><td class="content"><?=nl2br($row->content)?></td></tr>
+        <tr><td class="title"><?=htmlspecialchars($row->title)?></td></tr>
+        <tr><td class="content"><?=nl2br(htmlspecialchars($row->content))?></td></tr>
+        <?php if (!empty($file_name)): ?>
+        <tr>
+            <td class="file">
+                업로드 된 파일 :
+                <a href="/webapi/board/download.php?file=<?=urlencode($file_name)?>" download>
+                    <?=htmlspecialchars($file_name)?>
+                </a>
+            </td>
+        </tr>
+        <?php if ($is_image): ?>
+        <tr>
+            <td class="image-preview">
+                <p>미리보기:</p>
+                <img src="<?= $upload_path . urlencode($file_name) ?>" alt="이미지 미리보기" style="max-width: 500px; max-height: 400px;">
+            </td>
+        </tr>
+        <?php endif; ?>
+        <?php endif; ?>
     </table>
     <div class="button-container">
-    <a href="../index.php"><button type="button" class="button">홈으로</button></a>
-    <form method="post" action="/board/delete.php">
-        <input type="hidden" name="idx" value="<?=$row->idx?>">
-        <input type="submit" value="삭제" class="button2">
-    </form>
+        <a href="../../index.php"><button type="button" class="button">홈으로</button></a>
+        <form method="post" action="/webapi/board/delete.php">
+            <input type="hidden" name="idx" value="<?=$row->idx?>">
+            <input type="submit" value="삭제" class="button2">
+        </form>
     </div>
 </body>
 </html>
